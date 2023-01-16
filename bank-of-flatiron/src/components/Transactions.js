80d85c5ad8d3a175ad192481d3535fb1e33d61eb
Lorraineken <lorrainekupa@gmail.com>
2023-01-16 14:25:16 +0300

    Created components folder

---
+++ bank-of-flatiron/src/components/Transactions.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from "react";
+
+
+function Transactions(){
+
+    return(
+        <div>
+            
+        </div>
+    )
+
+}
+
+
+
+export default Transactions;
