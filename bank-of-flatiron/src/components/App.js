61f36ea657c9dbf14cf6125c4831c42a841bfcf0
Lorraineken <lorrainekupa@gmail.com>
2023-01-17 13:07:08 +0300

    Added FilterSearch component

--- bank-of-flatiron/src/components/App.js
+++ bank-of-flatiron/src/components/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import "./bootstrap.css"
 import Transactions from "./Transactions"
 import Form from "./Form";
+import SearchBar from "./SearchBar";
 
 function App() {
   
 
   return (
     <div >  
+      <SearchBar/>
     <Transactions />
     <Form />
     </div>
---
+++ bank-of-flatiron/src/components/FilterSearch.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FilterSearch(){
+
+    return(
+        <div>
+
+
+        </div>
+    )
+}
+
+export default FilterSearch
