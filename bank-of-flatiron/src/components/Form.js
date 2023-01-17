2ec5b028fbade4372b7ec6f96d311098229a457d
Lorraineken <lorrainekupa@gmail.com>
2023-01-17 04:35:33 +0300

    Made the form inputs stateful

--- bank-of-flatiron/src/components/Form.js
+++ bank-of-flatiron/src/components/Form.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React , {useState}from "react";
 
 function Form(){
+
+    const[date,setDate] = useState("2023-01-01")
+    const[description,setDescription] = useState("paid for rent")
+    const [category, setCategory] = useState("Housing")
+    const [amount, setAmount] = useState(500)
+
+ function handleDateChange(){}
 
 return (
     <div>
